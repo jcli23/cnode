e3c64b50ab1396409e753ca01b6ba5aa1725bfd7
jcli23 <623896069@qq.com>
2019-10-21 13:32:59 +0800

    cnode上传

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="home el__input__icon">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
   }
+
 }
 </script>
